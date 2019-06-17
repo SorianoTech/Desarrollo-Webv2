657d71e836a25d9c1ecb170502d5c7622e8c71d8
LocalSoriano <tecnologia@sergiosoriano.es>
2019-06-17 17:07:19 +0200

    Cajero

--- projects/PHP/cajero/index.php
+++ projects/PHP/cajero/index.php
@@ -15,7 +15,7 @@
     <?php 
           $cantidad = $_POST['cantidad'];
           $cantidad = $cantidad *100; //Convertimos la cantidad a un entero
-
+          $cantidad_eu = $cantidad/100;
         ?>
     <nav></nav>
   </header>  
@@ -33,79 +33,131 @@
         <img class="pure-img" src="">
         <i class="fas fa-euro-sign"></i>
         <?php 
-        echo $cantidad.' en céntimos'; //imprimimos la cantidad en centimos
+        echo $cantidad_eu; //imprimimos la cantidad 
 
-        //Biiletes y monedas 5000-2000-1000-500-200-100-50
+        //Billetes y monedas 5000-2000-1000-500-200-100-50
         //Monedas 200-100-50-10-5-2-1
-        if (($cantidad/50000) > 0){ //Existe billete de 500
-          $resto = $cantidad%50000; //Comprobamos el resto de los billetes de 500 para saber si necesitamos mas billetes
-          
-            if($resto>0){ //Quiere decir que tenemos que dar mas billetes NO EXACTO
-              $entero = $cantidad - $resto; //restamos el resto al importe para saber cuantos billetes exactos tenemos que dar
-              $bill_500 = $entero / 50000; //Calculo los billetes de 500 que tengo que dar
-              echo '<p>Te corresponden '.$bill_500.' billetes de 500€ NO EXACTOS</p>';
-              echo '<p>Te queda por repartir '.$resto.' céntimos de euro</p>';
+        if (($cantidad%50000) > 0){ //Existe billete de 500
+          $resto = $cantidad%50000; //Comprobamos el resto que tenemos que repartir
+          $bill_500 = ($cantidad - $resto)/50000; //restamos el resto al importe para saber cuantos billetes exactos tenemos que dar
+          echo '<p>'.$bill_500.' billetes de 500€ </p>';
+          $cantidad = $resto;
+          if($cantidad%20000>0){ //billetes de 200 no exactos
+            $resto = $cantidad%20000; //Comprobamos el resto que tenemos que repartir
+            $bill_200 = ($cantidad - $resto)/20000;
+            echo '<p>'.$bill_200.' billetes de 200€ </p>';
+            $cantidad = $resto;
+            if($cantidad%10000>0){//cantidad de billete de 100 no exactos
+              $resto = $cantidad%10000; //Comprobamos el resto que tenemos que repartir
+              $bill_200 = ($cantidad - $resto)/10000; 
+              echo '<p>'.$bill_200.' billetes de 100€ </p>';
               $cantidad = $resto;
-              if($cantidad%2000>0){ //billetes de 200 no exactos
-                $entero = $cantidad - $resto;
-                $bill_200 = $entero / 20000;
-                echo '<p>Te corresponden '.$bill_200.' billetes de 200€ NO EXACTOS</p>';
-                 echo '<p>Te queda por repartir '.$resto.' céntimos de euro</p>';
+                if($cantidad%5000>0){//cantidad de billete de 50 no exactos
+                $resto = $cantidad%5000; //Comprobamos el resto que tenemos que repartir
+                $bill_50 = ($cantidad - $resto)/5000; 
+                echo '<p>'.$bill_50.' billetes de 50€ </p>';
                 $cantidad = $resto;
-              }else{ //Billetes de 200 exactos
-                $bill_200 = $cantidad / 20000; //Calculo los billetes de 500 que tengo que dar
-                echo '<p>Te corresponden '.$bill_200.' billetes de 200€ EXACTOS</p>';
-              }
-            }else{ //En caso de que el resto me de 0 (numero exacto de billetes)
-              $bill_500 = $cantidad / 50000; //Calculo los billetes de 500 que tengo que dar
-              echo '<p>Te corresponden '.$bill_500.' billetes de 500€ EXACTOS</p>';
-            }
-        }
-        if(($cantidad/20000) > 0){ //Existe billete de 200)
-          echo '<h1>Billete de 200</h1>';
-        }
-        if(($cantidad/10000) > 0){ //Existe billete de 100)
-          echo 'Billete de 100';
-        }
-        if(($cantidad/5000) > 0){ //Existe billete de 50)
-          echo 'Billete de 50';
-        }
-        if(($cantidad/2000) > 0){ //Existe billete de 20)
-          echo 'Billete de 20';
-        }
-        if(($cantidad/1000) > 0){ //Existe billete de 10)
-          echo 'Billete de 10';
-        }
-        if(($cantidad/500) > 0){ //Existe billete de 5)
-          echo 'Moneda de 5';
-        }
-        if(($cantidad/200) > 0){ //Existe moneda de 2)
-          echo 'Moneda de 2';
-        }
-        if(($cantidad/100) > 0){ //Existe moneda de 1)
-          echo 'Moneda de 1';
-        }
-        if(($cantidad/50) > 0){ //Existe moneda de 50cent)
-          echo 'Monedas de 50 cent';
-        }
-        if(($cantidad/20) > 0){ //Existe moneda de 20cent)
-          echo 'Billete de 20 cent';
-        }
-        if(($cantidad/10) > 0){ //Existe moneda de 10cent)
-          echo 'Billete de 10 cent';
-        }
-        if(($cantidad/5) > 0){ //Existe moneda de 5cent)
-          echo 'Billete de 5 cent';
-        }
-        if(($cantidad/2) > 0){ //Existe moneda de 5cent)
-          echo 'Billete de 2 cent';
-        }
-        if(($cantidad/1) > 0){ //Existe moneda de 5cent)
-          echo 'Billete de 1 cent';
-        }
-        else{
-          echo '<p>Mierda, no tenemos dinero!! Consigue un poco y dime la cantidad que tienes</p>';
-        }
+                  if($cantidad%2000>0){//cantidad de billete de 20 no exactos
+                  $resto = $cantidad%2000; //Comprobamos el resto que tenemos que repartir
+                  $bill_20 = ($cantidad - $resto)/2000; 
+                  echo '<p>'.$bill_20.' billetes de 20€ </p>';
+                  $cantidad = $resto;
+                    if($cantidad%1000>0){//cantidad de billete de 10 
+                    $resto = $cantidad%1000; //Comprobamos el resto que tenemos que repartir
+                    $bill_10 = ($cantidad - $resto)/1000; 
+                    echo '<p>'.$bill_10.' billetes de 10€ </p>';
+                    $cantidad = $resto;
+                      if($cantidad%500>0){//cantidad de billete de 5 no exactos
+                      $resto = $cantidad%500; //Comprobamos el resto que tenemos que repartir
+                      $bill_5 = ($cantidad - $resto)/500; 
+                      echo '<p>'.$bill_5.' billetes de 5€ </p>';
+                      $cantidad = $resto;
+                        if($cantidad%200>0){//cantidad de monedas de 2 no exactos
+                        $resto = $cantidad%200; //Comprobamos el resto que tenemos que repartir
+                        $mon_2 = ($cantidad - $resto)/200; 
+                        echo '<p>'.$mon_2.' Monedas de 2€ </p>';
+                        $cantidad = $resto;
+                          if($cantidad%100>0){//cantidad de monedas de 1 no exactos
+                          $resto = $cantidad%100; //Comprobamos el resto que tenemos que repartir
+                          $mon_1 = ($cantidad - $resto)/100; 
+                          echo '<p>'.$mon_1.' Monedas de 1€ </p>';
+                          $cantidad = $resto;
+                            if($cantidad%50>0){//cantidad de monedas de 1 no exactos
+                            $resto = $cantidad%50; //Comprobamos el resto que tenemos que repartir
+                            $mon_05 = ($cantidad - $resto)/50; 
+                            echo '<p>'.$mon_05.' Monedas de 0.50€ </p>';
+                            $cantidad = $resto;
+                              if($cantidad%20>0){//cantidad de monedas de 0.20 no exactos
+                              $resto = $cantidad%20; //Comprobamos el resto que tenemos que repartir
+                              $mon_02 = ($cantidad - $resto)/20; 
+                              echo '<p>'.$mon_02.' Monedas de 0.20€ </p>';
+                              $cantidad = $resto;
+                                if($cantidad%10>0){//cantidad de monedas de 0.10 no exactos
+                                $resto = $cantidad%10; //Comprobamos el resto que tenemos que repartir
+                                $mon_01 = ($cantidad - $resto)/10; 
+                                echo '<p>'.$mon_01.' Monedas de 0.10€ </p>';
+                                $cantidad = $resto;
+                                  if($cantidad%5>0){//cantidad de monedas de 0.10 no exactos
+                                  $resto = $cantidad%5; //Comprobamos el resto que tenemos que repartir
+                                  $mon_005 = ($cantidad - $resto)/5; 
+                                  echo '<p>'.$mon_005.' Monedas de 0.05€ </p>';
+                                  $cantidad = $resto;
+                                    if($cantidad%2>0){//cantidad de monedas de 0.10 no exactos
+                                    $resto = $cantidad%2; //Comprobamos el resto que tenemos que repartir
+                                    $mon_002 = ($cantidad - $resto)/2; 
+                                    echo '<p>'.$mon_002.' Monedas de 0.02€ </p>';
+                                    $cantidad = $resto;
+                                      if($cantidad%1>0){//cantidad de monedas de 0.01 no exactos
+                                      $resto = $cantidad%1; //Comprobamos el resto que tenemos que repartir
+                                      $mon_001 = ($cantidad - $resto)/1; 
+                                      echo '<p>'.$mon_001.' Monedas de 0.01€ </p>';
+                                      $cantidad = $resto;
+                                      }else{ //Monedas de 0.01 exactos
+                                        $mon_001 = $cantidad / 1; //Calculo las monedas de 0.01 que tengo que dar
+                                        echo '<p>'.$mon_001.' monedas de 0.01€ </p>';}
+                                    }else{ //Monedas de 0.01 exactos
+                                      $mon_002 = $cantidad / 2; //Calculo las monedas de 0.02 que tengo que dar
+                                      echo '<p>'.$mon_002.' monedas de 0.02€ </p>';}
+                                  }else{ //Monedas de 0.05 exactos
+                                    $mon_005 = $cantidad / 5; //Calculo las monedas de 0.05 que tengo que dar
+                                    echo '<p>'.$mon_005.' monedas de 0.05€ </p>';}
+                                }else{ //Monedas de 0.10 exactos
+                                  $mon_01 = $cantidad / 10; //Calculo las monedas de 0.10 que tengo que dar
+                                  echo '<p>'.$mon_01.' monedas de 0.10€ </p>';}
+                              }else{ //Monedas de 0.20 exactos
+                                $mon_02 = $cantidad / 20; //Calculo las monedas de 0.20 que tengo que dar
+                                echo '<p>'.$mon_02.' monedas de 0.20€ </p>';}
+                            }else{ //Monedas de 0.50 exactos
+                              $mon_05 = $cantidad / 50; //Calculo las monedas de 0.50 que tengo que dar
+                              echo '<p>'.$mon_05.' monedas de 0.50€ </p>';}
+                          }else{ //Monedas de 1 exactas
+                            $mon_1 = $cantidad / 100; //Calculo las monedas de 1 que tengo que dar
+                            echo '<p>'.$mon_1.' monedas de 1€ </p>';}
+                        }else{ //Monedas de 2 exactas
+                          $mon_2 = $cantidad / 200; //Calculo los monedas de 2 que tengo que dar
+                          echo '<p>'.$mon_2.' monedas de 2€ </p>';}
+                      }else{ //Billetes de 5 exactos
+                        $bill_5 = $cantidad / 500; //Calculo los billetes de 5 que tengo que dar
+                        echo '<p>'.$bill_5.' billetes de 5€ </p>';}
+                    }else{ //Billetes de 10 exactos
+                      $bill_10 = $cantidad / 1000; //Calculo los billetes de 10 que tengo que dar
+                      echo '<p>'.$bill_10.' billetes de 10€ </p>';}
+                  }else{ //Billetes de 20 exactos
+                    $bill_20 = $cantidad / 2000; //Calculo los billetes de 20 que tengo que dar
+                    echo '<p>'.$bill_20.' billetes de 20€ </p>';}
+                }else{ //Billetes de 50 exactos
+                  $bill_50 = $cantidad / 5000; //Calculo los billetes de 50 que tengo que dar
+                  echo '<p>'.$bill_50.' billetes de 50€ </p>';}
+            }else{ //Billetes de 100 exactos
+              $bill_100 = $cantidad / 10000; //Calculo los billetes de 100 que tengo que dar
+              echo '<p>'.$bill_100.' billetes de 100€ </p>';}
+          }else{ //Billetes de 200 exactos
+            $bill_200 = $cantidad / 20000; //Calculo los billetes de 200 que tengo que dar
+            echo '<p>'.$bill_200.' billetes de 200€ </p>';}
+        }else{ //En caso de que el resto me de 0 (500)(numero exacto de billetes)
+          $bill_500 = $cantidad / 50000; //Calculo los billetes de 500 que tengo que dar
+          echo '<p>'.$bill_500.' billetes de 500€ </p>';}
+        
 
         ?>
       </div>
